ed72b1ddd098406fdbe1ed04266d87ed124392b1
hiroaki415 <hiroaki415@gmail.com>
2014-10-11 00:10:21 +0900

    'update'

--- setup.bat
+++ setup.bat
@@ -4,10 +4,19 @@ echo === Setup Dotfiles for Windows! ===
 echo making symlink for vim...
 mklink %HOMEDRIVE%%HOMEPATH%\.vimrc %HOMEDRIVE%%HOMEPATH%\dotfiles\.vimrc
 mklink %HOMEDRIVE%%HOMEPATH%\.gvimrc %HOMEDRIVE%%HOMEPATH%\dotfiles\.gvimrc
+echo symlink making done
 
 echo setup for vundle...
-mkdir %HOMEDRIVE%%HOMEPATH%\.vim
-mkdir %HOMEDRIVE%%HOMEPATH%\.vim\bundle
-git clone https://github.com/gmarik/Vundle.vim.git %HOMEDRIVE%%HOMEPATH%\.vim\bundle\vundle
+if exist %HOMEDRIVE%%HOMEPATH%\.vim goto SKIPVUNDLE
+        mkdir %HOMEDRIVE%%HOMEPATH%\.vim
+        mkdir %HOMEDRIVE%%HOMEPATH%\.vim\bundle
+        git clone https://github.com/gmarik/Vundle.vim.git %HOMEDRIVE%%HOMEPATH%\.vim\bundle\vundle
+        echo vundle setup done
+        goto VUNDLEDONE
+    :SKIPVUNDLE
+        echo vundle is already exist!
+        goto VUNDLEDONE
+:VUNDLEDONE
 
-echo Done!
+echo All Done!
+exit /b
