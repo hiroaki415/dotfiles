26493765a5fa6b1140d8981da29150a9252a6118
hiroaki415 <hiroaki415@gmail.com>
2014-10-10 22:56:53 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
+echo "=== Setup Dotfiles for Linux! ==="
+
+echo "making symlink for vim..."
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 
+echo "setup for vundle..."
 mkdir ~/.vim
 mkdir ~/.vim/bundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
 
-
+echo "making symlink for bash..."
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+
+echo "Done!"
