dedc019c6710bc89fd46bacb39d9d1f81555f080
hiroaki415 <hiroaki415@gmail.com>
2014-10-29 00:35:03 +0900

    update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -59,3 +59,8 @@
 
 (require 'yasnippet)
 (yas-global-mode 1)
+
+(require 'direx)
+(push '(direx:direx-mode :position left :width 25 :dedicated t)
+      popwin:special-display-config)
+(global-set-key (kbd "C-x C-j") 'direx:jump-to-directory)
