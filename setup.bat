d05d36c44881dc0bcefe5299defb167469cc3492
hiroaki415 <hiroaki415@gmail.com>
2014-11-04 15:26:38 +0900

    update

--- setup.bat
+++ setup.bat
@@ -1,14 +1,19 @@
 @echo off
 echo === Setup Dotfiles for Windows! ===
 
-echo making symlink for vim...
-mklink %HOMEDRIVE%%HOMEPATH%\.vimrc %HOMEDRIVE%%HOMEPATH%\dotfiles\.vimrc
-mklink %HOMEDRIVE%%HOMEPATH%\.gvimrc %HOMEDRIVE%%HOMEPATH%\dotfiles\.gvimrc
-echo symlink making done
+if %ERRORLEVEL% EQU 0 (
+    echo making symlink for vim...
+    mklink %HOMEDRIVE%%HOMEPATH%\.vimrc %HOMEDRIVE%%HOMEPATH%\dotfiles\.vimrc
+    mklink %HOMEDRIVE%%HOMEPATH%\.gvimrc %HOMEDRIVE%%HOMEPATH%\dotfiles\.gvimrc
+    echo symlink making done
 
-echo setup for vundle...
-git clone https://github.com/gmarik/Vundle.vim.git %HOMEDRIVE%%HOMEPATH%\dotfiles\.vim\bundle\vundle
-echo vundle setup done
+    echo setup for vundle...
+    git clone https://github.com/gmarik/Vundle.vim.git %HOMEDRIVE%%HOMEPATH%\dotfiles\.vim\bundle\vundle
+    echo vundle setup done
+) ELSE (
+    echo this script must be run by Administrator
+    pause
+)
 
 echo All Done!
 exit /b
