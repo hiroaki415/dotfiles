6b161fa07691c99c821764b39f4a64081e0d24d6
hiroaki415 <hiroaki415@gmail.com>
2024-10-17 15:10:58 +0900

    add some nginx funcs for ps

--- MyProfile.ps1
+++ MyProfile.ps1
@@ -1,5 +1,31 @@
 $theme = "slimfat.omp.json"
 oh-my-posh init pwsh --config $env:POSH_THEMES_PATH/$theme | Invoke-Expression
 
+
+# disable system startup service of nginx
 $NGINX_DIR = "C:\tools\nginx-1.27.2"
-Set-Alias -Name nginx -Value $NGINX_DIR\nginx.lnk
+function nginx {
+    $exePath = "$NGINX_DIR\nginx.exe"
+    $startInfo = New-Object System.Diagnostics.ProcessStartInfo
+    $startInfo.FileName = $exePath
+    $startInfo.Arguments = $args
+    $startInfo.WorkingDirectory = $NGINX_DIR
+    $startInfo.RedirectStandardOutput = $true
+    $startInfo.UseShellExecute = $false
+    $process = [System.Diagnostics.Process]::Start($startInfo)
+    $output = $process.StandardOutput.ReadToEnd()
+    Write-Host $output
+    $process.WaitForExit()
+}
+
+function start-nginx {
+    $exePath = "$NGINX_DIR\nginx.exe"
+    $startInfo = New-Object System.Diagnostics.ProcessStartInfo
+    $startInfo.FileName = $exePath
+    $startInfo.Arguments = $args
+    $startInfo.WorkingDirectory = $NGINX_DIR
+    $startInfo.RedirectStandardOutput = $false
+    $startInfo.UseShellExecute = $true
+    $process = [System.Diagnostics.Process]::Start($startInfo)
+    Write-Host "Nginx started in the background with PID: $($process.Id)"
+}
