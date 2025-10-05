7f798eebf1057446d7e4484efabfd381fc2b5e18
hiroaki415 <hiroaki415@gmail.com>
2025-10-05 23:12:43 +0900

    modify /sakura a little bit

---
+++ sakura/plugins/DevUtils/TestCommand.js
@@ -0,0 +1,9 @@
+function testCommand() {
+//    Write Test Script Here
+//
+//    Editor.MessageBox('Hello World!');
+//    Editor.StatusMsg('Hello World!');
+//    Editor.TraceOut('Hello World!');
+}
+
+testCommand();
