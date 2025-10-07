112789918f5c49f6b722decce19ab72114f9f987
hiroaki415 <hiroaki415@gmail.com>
2025-10-07 20:51:30 +0900

    fix some bugs

--- sakura/plugins/DevUtils/Config.js
+++ sakura/plugins/DevUtils/Config.js
@@ -14,13 +14,47 @@ eval(loadModule('/plugins/DevUtils/Utility.js'));
 function Config() {
 
     this.raw = null;
+    this.types = {};
 
     this.load = function() {
         var root = Utility.getRootDir();
+        var fso = new ActiveXObject('Scripting.FileSystemObject');
         var file = fso.OpenTextFile(root + '/sakura.ini', 1);
         this.raw = file.ReadAll();
         file.Close();
         file = null;
+
+        this.parseRaw();
+
+        TraceOut(this.types['C/C++']['szTypeExts']);
+
+    };
+
+    this.parseRaw = function(ctype) {
+
+        var lines = this.raw.split(/\r\n/);
+        var type = {szTypeName:'dummy'};
+
+        for (var i in lines) {
+
+            if (/\[Types\(\d+\)\]/.test(lines[i])) {
+                this.types[type.szTypeName] = type;
+                type = {szTypeName:'dummy'};
+            }
+
+            var index = lines[i].indexOf("=");
+            var key = lines[i].substring(0, index);
+            var value = lines[i].substring(index + 1);
+
+            if (false) {
+                //
+            } else {
+                type[key] = value;
+            }
+
+            if (Number(i) === (lines.length - 1) ) { this.types[type.szTypeName] = type; }
+
+        }
     };
 
     this.getLineComment = function(ctype) {
