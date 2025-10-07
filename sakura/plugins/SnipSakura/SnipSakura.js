6cd474eaf5d059ccdeea24e3abbeaaaec6b39ca7
hiroaki415 <hiroaki415@gmail.com>
2025-10-07 21:02:11 +0900

    add SnipSakura plugin for snippet management

---
+++ sakura/plugins/SnipSakura/SnipSakura.js
@@ -0,0 +1,29 @@
+var fso = new ActiveXObject('Scripting.FileSystemObject');
+var pluginDir = Plugin.GetPluginDir();
+var root = fso.GetParentFolderName(fso.GetParentFolderName(pluginDir));
+var file = fso.OpenTextFile(root + '/plugins/DevUtils/LoadModule.js', 1);
+var loadModuleRaw = file.ReadAll();
+file.Close();
+file = null;
+fso = null;
+
+eval(loadModuleRaw);
+eval(loadModule('/plugins/DevUtils/Decorator.js'));
+eval(loadModule('/plugins/DevUtils/Cursor.js'));
+eval(loadModule('/plugins/DevUtils/Config.js'));
+eval(loadModule('/plugins/DevUtils/Utility.js'));
+
+
+function SnipSakura() {
+    
+}
+
+
+(function() {
+    var cmd = Plugin.GetCommandNo();
+    switch (cmd) {
+        case 1:
+            CommandDecorator(SnipSakura)();
+            break;
+    }
+})();
