4df33d7e6c9f1ee231d9b0dede78a8bc6b60eaf2
hiroaki415 <hiroaki415@gmail.com>
2026-06-22 22:46:14 +0900

    update CommandListener (support regexp and load external config)

--- sakura/plugins/CommandListener/CommandListener.js
+++ sakura/plugins/CommandListener/CommandListener.js
@@ -13,6 +13,14 @@ eval(loadModule('/plugins/DevUtils/Utility.js'));
 
 function CommandListener() {
     var comConf = Utility.evalAsObject(loadModule('/plugins/CommandListener/CommandListenerConfig.json'));
+    if (Utility.isArray(comConf.externals)) {
+        for (var i = 0; i < comConf.externals.length; i++) {
+            var tmpConf = Utility.evalAsObject(loadModule(comConf.externals[i]));
+            comConf.modules = comConf.modules.concat(tmpConf.modules);
+            comConf.preload = comConf.preload.concat(tmpConf.preload);
+            comConf.handlers = comConf.handlers.concat(tmpConf.handlers);
+        }
+    }
     for (var i = 0; i < comConf.modules.length; i++) {
         eval(loadModule(comConf.modules[i]));
     }
@@ -20,7 +28,19 @@ function CommandListener() {
         eval(comConf.preload[i]);
     }
     var cmd = Editor.InputBox('Input Command', '', 255);
-    eval(comConf.commands[cmd]);
+    for (var key in comConf.commands) {
+        var matches = cmd.match(new RegExp('^' + key + '$'));
+        if (matches) {
+            if (typeof(comConf.commands[key]) === 'string') {
+                eval(comConf.commands[key]);
+            } else if (Utility.isArray(comConf.commands[key])) {
+                for (var i = 0; i < comConf.commands[key].length; i++) {
+                    eval(comConf.commands[key][i]);
+                }
+            }
+            eval(comConf.commands[key]);
+        }
+    }
 }
 
 
