14fe1f77579370dc3738fd27b08792a19ee82678
hiroaki415 <hiroaki415@gmail.com>
2026-06-25 18:29:50 +0900

    fix wrong variable name

--- sakura/setup.ps1
+++ sakura/setup.ps1
@@ -5,6 +5,6 @@
 $target = "$HOME\dotfiles\sakura\"
 $path = "$HOME\AppData\Roaming\sakura\"
 if (Test-Path $path) {
-    Remove-Item -Path $dest
+    Remove-Item -Path $path
 }
 New-Item -ItemType SymbolicLink -Path $path -Value $target
